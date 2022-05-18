aae380ba9f2ceb5e87467629fbc546e8a6b0f241
mmar58 <81753445+mmar58@users.noreply.github.com>
2022-05-18 14:13:19 +0600

    Update Datefunctions.js
    Added print SQL datetime function

--- Datefunctions.js
+++ Datefunctions.js
@@ -38,10 +38,51 @@ function printdatetime(date) {
 //testing
 console.log(printdatetime(new Date()))
 
+//Datetime in Sql datetime format function
+function printSQLdatetime(date) {
+    var outputtime = ''
+    if (date.getFullYear() < 10) {
+        outputtime = outputtime + '0' + date.getFullYear() + "-"
+    } else {
+        outputtime = outputtime + date.getFullYear() + "-"
+    }
+    if (date.getMonth() < 10) {
+        outputtime = outputtime + '0' + date.getMonth() + "-"
+    } else {
+        outputtime = outputtime + date.getMonth() + "-"
+    }
+    if (date.getDate() < 10) {
+        outputtime = outputtime + '0' + date.getDate() + " "
+    } else {
+        outputtime = outputtime + date.getDate() + " "
+    }
+
+    if (date.getHours() < 10) {
+        outputtime = outputtime + '0' + date.getHours() + ":"
+    } else {
+        outputtime = outputtime + date.getHours() + ":"
+    }
+    if (date.getMinutes() < 10) {
+        outputtime = outputtime + '0' + date.getMinutes() + ":"
+    } else {
+        outputtime = outputtime + date.getMinutes() + ":"
+    }
+    if (date.getSeconds() < 10) {
+        outputtime = outputtime + '0' + date.getSeconds() + " "
+    } else {
+        outputtime = outputtime + date.getSeconds() + " "
+    }
+
+    return outputtime;
+}
+
+//testing
+console.log(printSQLdatetime(new Date()))
+
 //Add hours to date
 function addHours(date,hours) {
     date.setTime(date.getTime() + hours * 60 * 60 * 1000);
     return date;
-}
-//testing
-console.log(printdatetime(addHours(new Date(),2)))
+  }
+  //testing
+  console.log(printdatetime(addHours(new Date(),2)))
